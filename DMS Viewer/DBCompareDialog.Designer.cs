0953d2819a645edfb48aeaa9de3f0997b79dbf3e
Timothy Slater <tslater2006@gmail.com>
2019-01-18 08:21:11 -0600

    Adjust UI layout, and where dialogs open

--- DMS Viewer/DBCompareDialog.Designer.cs
+++ DMS Viewer/DBCompareDialog.Designer.cs
@@ -34,31 +34,35 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 73);
+            this.progressBar1.Location = new System.Drawing.Point(6, 38);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(775, 42);
+            this.progressBar1.Size = new System.Drawing.Size(388, 22);
             this.progressBar1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Running compare...";
             // 
             // DBCompareDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 144);
+            this.ClientSize = new System.Drawing.Size(400, 75);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DBCompareDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Compare Runner";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DBCompareDialog_FormClosing);
             this.Load += new System.EventHandler(this.DBCompareDialog_Load);
--- DMS Viewer/DBLogin.Designer.cs
+++ DMS Viewer/DBLogin.Designer.cs
@@ -40,65 +40,58 @@
             // 
             // txtDBName
             // 
-            this.txtDBName.Location = new System.Drawing.Point(172, 57);
-            this.txtDBName.Margin = new System.Windows.Forms.Padding(6);
+            this.txtDBName.Location = new System.Drawing.Point(86, 30);
             this.txtDBName.Name = "txtDBName";
-            this.txtDBName.Size = new System.Drawing.Size(266, 31);
+            this.txtDBName.Size = new System.Drawing.Size(135, 20);
             this.txtDBName.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 63);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(21, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "DB Name:";
             // 
             // txtDBUser
             // 
-            this.txtDBUser.Location = new System.Drawing.Point(172, 100);
-            this.txtDBUser.Margin = new System.Windows.Forms.Padding(6);
+            this.txtDBUser.Location = new System.Drawing.Point(86, 52);
             this.txtDBUser.Name = "txtDBUser";
-            this.txtDBUser.Size = new System.Drawing.Size(266, 31);
+            this.txtDBUser.Size = new System.Drawing.Size(135, 20);
             this.txtDBUser.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 106);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(13, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "User Name:";
             // 
             // txtDBPass
             // 
-            this.txtDBPass.Location = new System.Drawing.Point(172, 143);
-            this.txtDBPass.Margin = new System.Windows.Forms.Padding(6);
+            this.txtDBPass.Location = new System.Drawing.Point(86, 74);
             this.txtDBPass.Name = "txtDBPass";
             this.txtDBPass.PasswordChar = '*';
-            this.txtDBPass.Size = new System.Drawing.Size(266, 31);
+            this.txtDBPass.Size = new System.Drawing.Size(135, 20);
             this.txtDBPass.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 149);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(21, 77);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Password:";
             // 
             // btnDBConnect
             // 
-            this.btnDBConnect.Location = new System.Drawing.Point(153, 203);
-            this.btnDBConnect.Margin = new System.Windows.Forms.Padding(6);
+            this.btnDBConnect.Location = new System.Drawing.Point(76, 106);
             this.btnDBConnect.Name = "btnDBConnect";
-            this.btnDBConnect.Size = new System.Drawing.Size(180, 44);
+            this.btnDBConnect.Size = new System.Drawing.Size(90, 23);
             this.btnDBConnect.TabIndex = 21;
             this.btnDBConnect.Text = "Connect";
             this.btnDBConnect.UseVisualStyleBackColor = true;
@@ -107,18 +100,17 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 9);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(22, 5);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(407, 25);
+            this.label4.Size = new System.Drawing.Size(202, 13);
             this.label4.TabIndex = 22;
             this.label4.Text = "You MUST provide bootstrap credentials.";
             // 
             // DBLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 261);
+            this.ClientSize = new System.Drawing.Size(251, 136);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnDBConnect);
             this.Controls.Add(this.txtDBPass);
@@ -128,9 +120,11 @@
             this.Controls.Add(this.txtDBName);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DBLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Database Login";
             this.ResumeLayout(false);
             this.PerformLayout();
